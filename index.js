26c96e205ea918ab217714bc451faa88040c9277
Swaraj Patil <swarajpatil1703@gmil.com>
2024-01-22 19:32:25 +0530

    First commit: Initialized the repository

---
+++ index.js
@@ -0,0 +1,55 @@
+// Question 1
+// Provide a solution based on the instructions to get the desired output
+function squareRootCalculator (number) {
+    // Write your code here
+
+    return 0
+}
+
+
+// Question 2
+// Provide a solution based on the instructions to get the desired output
+function arraySum (numbers) {
+    // Write your code here
+
+    return 0
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Read-Only content:
+const q1s1 = document.querySelector('.q1s1')
+q1s1.innerHTML += squareRootCalculator(81)
+q1s1.innerHTML === 'Output: 9' ? q1s1.style.color = 'green' : q1s1.style.color = 'red'
+const q1s2 = document.querySelector('.q1s2')
+q1s2.innerHTML += squareRootCalculator(144)
+q1s2.innerHTML === 'Output: 12' ? q1s2.style.color = 'green' : q1s2.style.color = 'red'
+
+const q2s1 = document.querySelector('.q2s1')
+q2s1.innerHTML += arraySum([1, 2, 3, 4, 10, 11])
+q2s1.innerHTML === 'Output: 31' ? q2s1.style.color = 'green' : q2s1.style.color = 'red'
+const q2s2 = document.querySelector('.q2s2')
+q2s2.innerHTML += arraySum([30, 50, 20, 100])
+q2s2.innerHTML === 'Output: 200' ? q2s2.style.color = 'green' : q2s2.style.color = 'red'
